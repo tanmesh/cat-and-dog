c45942776b533b565c9e68e44c5b2292724bfad5
tanmesh <tanmeshnm@gmail.com>
2019-01-12 20:35:46 +0530

    model runned

--- img_cla.py
+++ img_cla.py
@@ -1,74 +1,90 @@
 import numpy as np
-from keras.layers import Activation
+from keras.layers import Activation, Dropout
 from keras.layers import Conv2D
 from keras.layers import Dense
-from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.layers import MaxPooling2D
 from keras.models import Sequential
 from keras_preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 
-from prepare_data import prepare_data
+from prepare_data import prepare_data, split_data
 
 
 def img_classi():
-    x, y = prepare_data()
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_size=1)
+    print("Splitting data into train and test...")
+    train_images_dogs_cats, test_images_dogs_cats = split_data()
+    img_width = 150
+    img_height = 150
+
+    print("Preparing the train data...")
+    x, y = prepare_data(train_images_dogs_cats, img_width, img_height)
+    print("Splitting the train data into training and validation set...")
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=1)
     n_train = len(x_train)
     n_val = len(x_val)
-    batch_size = 16
-    img_width = img_height = 50
 
-    print("Building model..")
+    batch_size = 16
+
+    print("Building the model..")
     model = Sequential()
+
+    print("Running the first layer...")
     model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
+    print("Running the second layer...")
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
+    print("Running the third layer...")
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
+    print("Running the last layer...")
     model.add(Flatten())
     model.add(Dense(64))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    # try:
+    #     model.add(Dropout(0.5))
+    # except Exception as e:
+    #     print("There is error........."+str(e))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
 
+    print("Compiling the model...")
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    print("model builed...")
+
+    print("Model build.")
 
     print('Data augmentation...')
-    train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-    val_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+    train_data_gen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+    val_data_gen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
-    print('Preparing data for training and validation set...')
-    train_generator = train_datagen.flow(np.array(x_train), y_train, batch_size=batch_size)
-    validation_generator = val_datagen.flow(np.array(x_val), y_val, batch_size=batch_size)
+    print('Preparing generators for training and validation sets...')
+    train_generator = train_data_gen.flow(np.array(x_train), y_train, batch_size=batch_size)
+    validation_generator = val_data_gen.flow(np.array(x_val), y_val, batch_size=batch_size)
 
-    print('Training starts...')
-    model.fit_generator(train_generator, steps_per_epoch=n_train,
-                        epochs=30, validation_data=validation_generator, validation_steps=n_val)
-    print('Training completed...')
+    print('Fitting the model...')
+    model.fit_generator(train_generator, steps_per_epoch=n_train // batch_size, epochs=30,
+                        validation_data=validation_generator, validation_steps=n_val // batch_size)
 
-    print('Savinf the model...')
+    print('Saving the model...')
     model.save_weights('model_wieghts.h5')
     model.save('model_keras.h5')
     print("Model saved...")
 
     print('Generating test data...')
-    x_test, y_test = prepare_data()
-    # test_datagen = ImageDataGenerator(rescale=1. / 255)
-    test_generator = val_datagen.flow(np.array(x_test), bsatch_size=batch_size)
-    pred = model.predict_generator(test_generator, verbose=1)
+    x_test, y_test = prepare_data(test_images_dogs_cats, img_width, img_height)
+    test_data_gen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_data_gen.flow(np.array(x_test), batch_size=batch_size)
 
-    print(pred)
+    print("Predicting...")
+    pred = model.predict_generator(test_generator, verbose=1, steps=len(test_generator))
+    print("Prediction is " + str(pred))
 
 
 img_classi()
--- prepare_data.py
+++ prepare_data.py
@@ -12,72 +12,32 @@ def natural_keys(text):
     return [atoi(c) for c in re.split('(\d+)', text)]
 
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder, filename))
-        if img is not None:
-            images.append(img)
-    return images
-
-
-def update(file_name):
-    str_train = '/Users/tanmesh/dev/cat_and_dog/dataset/train'
-    str_test = '/Users/tanmesh/dev/cat_and_dog/dataset/test'
-    new_file = []
-    for name in file_name:
-        if name.find(str_train) != -1:
-            new_file.append(name.replace(str_train, ''))
-        if name.find(str_test) != -1:
-            new_file.append(name.replace(str_test, ''))
-    return new_file
-
-
-def data(list_of_images, img_width, img_height):
+def prepare_data(list_of_images_path, img_width, img_height):
     x = []  # images as arrays
     y = []  # labels
-    cnt_exp = 0
-    for image in list_of_images:
-        try:
-            tmp = cv2.resize(cv2.imread(image), (img_width, img_height), interpolation=cv2.INTER_CUBIC)
-            x.append(tmp)
-        except Exception:
-            # print("There is some exception")
-            cnt_exp += 1
-            pass
+    for image_path in list_of_images_path:
+        read_image = cv2.imread(image_path)
+        tmp = cv2.resize(read_image, (img_width, img_height), interpolation=cv2.INTER_CUBIC)
+        x.append(tmp)
 
-    for i in list_of_images:
+    for i in list_of_images_path:
         if 'dog' in i:
             y.append(1)
         elif 'cat' in i:
             y.append(0)
-        # else:
-        # print('neither cat nor dog name present in images')
 
-    print("cnt_exp = " + str(cnt_exp))
     return x, y
 
 
-def prepare_data():
-    img_width = 150
-    img_height = 150
-    train_dir = '/Users/tanmesh/dev/cat_and_dog/dataset/train'
-    test_dir = '/Users/tanmesh/dev/cat_and_dog/dataset/test'
+def split_data():
+    train_dir = '/Users/tanmesh/dev/cat_and_dog/dataset/train/'
+    test_dir = '/Users/tanmesh/dev/cat_and_dog/dataset/test/'
     train_images_dogs_cats = [train_dir + i for i in os.listdir(train_dir)]  # use this for full dataset
     test_images_dogs_cats = [test_dir + i for i in os.listdir(test_dir)]
-
-    train_images_dogs_cats = update(train_images_dogs_cats)
-    test_images_dogs_cats = update(test_images_dogs_cats)
 
     train_images_dogs_cats.sort(key=natural_keys)
     train_images_dogs_cats = train_images_dogs_cats[0:1300] + train_images_dogs_cats[12500:13800]
     test_images_dogs_cats.sort(key=natural_keys)
 
-    x, y = data(train_images_dogs_cats, img_width, img_height)
+    return train_images_dogs_cats, test_images_dogs_cats
 
-    print("x = " + str(len(x)))
-    print("y = " + str(len(y)))
-    return x, y
-
-
-prepare_data()
